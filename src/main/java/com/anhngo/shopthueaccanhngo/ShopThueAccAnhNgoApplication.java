dfeea3e440272c188b1eb79846a0375b97e53d47
Anh Anh <ngovietanh121@gmail.com>
2024-08-24 10:48:26 +0700

    first init

---
+++ src/main/java/com/anhngo/shopthueaccanhngo/ShopThueAccAnhNgoApplication.java
@@ -0,0 +1,13 @@
+package com.anhngo.shopthueaccanhngo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopThueAccAnhNgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopThueAccAnhNgoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anhngo/shopthueaccanhngo/ShopThueAccAnhNgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhngo.shopthueaccanhngo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopThueAccAnhNgoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
